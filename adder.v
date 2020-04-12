92aec0b99e467ca1433728fbd237c8b18af5de9c
botrote <symbol0328@gmail.com>
2020-04-12 12:43:21 +0900

    adder 컴파일 오류 해결

--- adder.v
+++ adder.v
@@ -1,15 +1,16 @@
 /*
 	Adder for Updating PC
 */
+`include "opcodes.v" 
 
 module adder (
 	input [`WORD_SIZE-1:0] input1,
 	input [`WORD_SIZE-1:0] input2,
 
-	output [`WORD_SIZE-1:0] ouput
+	output [`WORD_SIZE-1:0] ouput1
 );
 
-	assign output = input1 + input2;
+	assign output1 = input1 + input2;
 
 endmodule
 
