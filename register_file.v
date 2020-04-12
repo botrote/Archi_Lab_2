3b6c3a9927c71cf3723bb700762fbdb0f59a6ff8
botrote <symbol0328@gmail.com>
2020-04-12 13:01:13 +0900

    registers 컴파일 에러 해결

--- register_file.v
+++ register_file.v
@@ -1,16 +1,17 @@
+`include "opcodes.v"
 
 module register_file (
-	intput [2:0] read_register_1, read_register_2,
+	input [2:0] read_register_1, read_register_2,
 	input [`WORD_SIZE-1:0] write_data,
 	input [2:0] write_register,
 	input signal_RegWrite,
 	input clk,
 	input reset_n,
 
-	output reg [`WORD_SIZE-1:0] read_data_1, read_data_2,
+	output [`WORD_SIZE-1:0] read_data_1, read_data_2
 );
 
-	reg [`WORD_SIZE-1:0] Registers [`NUM_REGS - 1:0]
+	reg [`WORD_SIZE-1:0] Registers [`NUM_REGS - 1:0];
 
 	assign read_data_1 = Registers[read_register_1];
 	assign read_data_2 = Registers[read_register_2];
@@ -19,7 +20,7 @@ module register_file (
     always @ (posedge clk or posedge reset_n) begin
     	if(reset_n)
     		for(i = 0; i < 4; i = i + 1)
-    			Registers <= 0;
+    			Registers[i] <= 0;
     	else if(signal_RegWrite && write_register != 0)
     		Registers[write_register] <= write_data;
     end
