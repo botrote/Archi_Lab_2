2d11b0d0916e3759a4625944d05ae3232891d5b4
shchae7 <seunghyunchae@chaeseunghyeons-MacBook-Pro.local>
2020-04-18 18:43:50 +0900

    init

--- cpu.v
+++ cpu.v
@@ -1,101 +1,215 @@
-`include "opcodes.v" 	   
+`include "opcodes.v"
+`timescale 1ns/100ps
 
 module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
-    output readM;
-    output writeM;
-    output [`WORD_SIZE-1:0] address;
-    inout [`WORD_SIZE-1:0] data;
-    input ackOutput;
-    input inputReady;
-    input reset_n;
-    input clk;
+	output readM;
+	output writeM;
+	output [`WORD_SIZE-1:0] address;
+	inout [`WORD_SIZE-1:0] data;
+	input ackOutput;
+	input inputReady;
+	input reset_n;
+	input clk;
 
-    reg stage; // input or output (Memory Write or Read)
+	reg readM;
+	reg writeM;
+	reg [`WORD_SIZE -1:0] address;
+	reg [3:0]opcode;
+	reg [1:0]rs;
+	reg [1:0]rt;
+	reg [1:0]rd;
+	reg [5:0]func;
+	reg [7:0]imm;
+	reg [11:0]jumpimm;
+	reg [`WORD_SIZE -1 :0]se;
+	reg [`WORD_SIZE - 1:0]regi[3:0];
+	reg [`WORD_SIZE - 1:0]pc;
+	reg write_data;
+	integer i;
 
-    // instruction elements
-    wire [3:0] opcode;
-    wire [1:0] rs;
-    wire [1:0] rt;
-    wire [1:0] rd;
-    wire [5:0] func;
-    wire [7:0] imm;
-    wire [11:0] target_address;
+	function [`WORD_SIZE-1:0]ARS;
+		input [`WORD_SIZE-1:0]A;
+		begin
+			if(A[`WORD_SIZE-1]==1)
+				ARS=((A>>1)+16'h8000);
+			else
+				ARS=(A>>1);
+		end
+	endfunction
 
-    wire [`WORD_SIZE - 1:0] extended_imm;
-    wire [`WORD_SIZE - 1:0] shift_result;
+	initial begin
+		pc = 16'h000;
+		for (i = 0; i< 4; i = i + 1)begin
+			regi[i] = 16'h0000;
+		end
+		address = 16'h0000;
+		write_data = 0;
+	end
 
-    wire [2:0] ALUOp;
-    wire RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite; // control signals
+	assign data = write_data ? regi[rt] : `WORD_SIZE'bz;
 
-    // MUX output
-    wire [1:0] mux1_output;
-    wire [`WORD_SIZE - 1:0] mux2_output;
-    wire [`WORD_SIZE - 1:0] mux3_output;
-    wire [`WORD_SIZE - 1:0] mux4_output;
-
-    // register input, output
-    wire [`WORD_SIZE - 1:0] read_data_1, read_data_2;
-    wire [`WORD_SIZE - 1:0] write_data;
-
-    // ALU input, output
-    wire zero;
-    wire [`WORD_SIZE - 1:0] ALU_result;
-
-    reg [`WORD_SIZE - 1:0] pc;
-    wire [`WORD_SIZE - 1:0] pc_4;
-    wire [`WORD_SIZE - 1:0] other_pc;
-    assign pc_4 = pc + 16'h4;
-
-    assign opcode = data[15:12];
-    assign rs = data[11:10];
-    assign rt = data[9:8];
-    assign rd = data[7:6];
-    assign func = data[5:0];
-    assign imm = data[7:0];
-    assign target_address = data[11:0];
-
-    control_unit control_unit1(opcode, func, RegDst, Jump, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
-
-    sign_extender sign_extender1(imm, extended_imm);
-    shift_left_2 shift_left_2(extended_imm, shift_result);
-
-    adder adder2(pc_4, shift_result, other_pc); // PC = PC + imm
-
-    multiplexer2 mux1(rd, rt, RegDst, mux1_output); // register input MUX
-    multiplexer mux2(read_data_2, extended_imm, ALUSrc, mux2_output); // ALU input MUX
-
-    wire mux3_control_signal;
-    assign mux3_control_signal = Branch & zero;
-    multiplexer mux3(pc_4, other_pc, mux3_control_signal, mux3_output); // PC + 4 MUX
-    multiplexer mux4(mux3_output, ALU_result, Jump, mux4_output); // other PC MUX
-
-    register_file register(rs, rt, write_data, mux1_output, RegWrite, clk, reset_n, read_data_1, read_data_2);
-
-    alu alu1(ALUOp, read_data_1, mux2_output, opcode, zero, ALU_result);
-
-    assign readM = MemRead;
-    assign writeM = MemWrite;
-    assign address = (stage) ? ALU_result : pc;
-    assign data = (writeM) ? ALU_result : 'bz;
-    //$display("data value %h", data);
-
-    initial begin
-	stage = 0;
-	pc = 0;
-    end
-
-    always @(*) begin
-        if(ackOutput == 1)
-            stage <= 0; // IF stage
-        else if(inputReady == 1)
-            stage <= 1; // MEM access stage
-    end
-
-    always @(posedge clk or posedge reset_n) begin
-	if(reset_n)
-	    pc <= 0;
-	else
-	    pc <= mux4_output;
-    	    $display("pc update value is %h", mux4_output);
-    end
+	always @(posedge clk) begin
+		address = pc;
+		readM = 1;
+		wait(inputReady);
+		readM = 0;
+		opcode = data[`WORD_SIZE - 1: 12];
+		rs = data[11: 10];
+		rt = data[9: 8];
+		rd = data[7: 6];
+		func = data[5:0];
+		imm = data[7:0];
+		jumpimm = data[11:0];
+		se = 16'h0000;
+		pc = pc + 1;
+		wait(!inputReady);
+		case(opcode)
+			`ADI_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				regi[rt] = (regi[rs] + se);
+				end//4'd4
+			`ORI_OP	:begin
+				se = (se | imm);
+				regi[rt] = (regi[rs] | se);
+				end	//4'd5
+			`LHI_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				regi[rt] = se;
+				end	//4'd6
+			`LWD_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				address = (regi[rs] + se);
+				readM = 1;
+				wait(inputReady);
+				readM = 0;
+				regi[rt] = data;
+				wait(!inputReady);
+			end	//4'd7
+			`SWD_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				address = (regi[rs] + se);
+				write_data = 1;
+				writeM = 1;
+				wait(ackOutput);
+				writeM = 0;
+				write_data = 0;
+				wait(!ackOutput);
+				end	//4'd8
+			`BNE_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				if(regi[rs] != regi[rt])begin
+					pc = (pc + se);
+				end
+			end	//4'd0
+			`BEQ_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				if(regi[rs] == regi[rt])begin
+					pc = (pc + se);
+				end
+			end	//4'd1
+			`BGZ_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				if( (regi[rs][`WORD_SIZE - 1] == 0) && (regi[rs] != 0) )begin
+					pc = (pc + se);
+				end
+			end	//4'd2
+			`BLZ_OP	:begin
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				if( (regi[rs][`WORD_SIZE - 1] == 1) && (regi[rs] != 0) )begin
+					pc = (pc + se);
+				end
+			end	//4'd3
+			`JMP_OP	:begin
+				se = (se | jumpimm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				pc = (pc & 16'hf000);
+				pc = (pc | se);
+			end	//4'd9
+			`JAL_OP :begin
+				regi[2] = pc;
+				se = (se | imm);
+				se = (se << 8);
+				for(i = 0; i< 8; i = i + 1) begin
+					se = ARS(se);
+				end
+				pc = (pc & 16'hf000);
+				pc = (pc | se);
+			end	//4'd10
+			default :begin
+				case(func)
+					`FUNC_ADD : begin
+						regi[rd] = (regi[rs] + regi[rt]);
+					end	//3'b000
+					`FUNC_SUB : begin
+						regi[rd] = (regi[rs] - regi[rt]);
+					end //3'b001
+					`FUNC_AND : begin
+						regi[rd] = (regi[rs] & regi[rt]);
+					end	//3'b010
+					`FUNC_ORR :	begin
+						regi[rd] = (regi[rs] | regi[rt]);
+					end//3'b011
+					`FUNC_NOT :	begin
+						regi[rd] = ~regi[rs];
+					end	//3'b100
+					`FUNC_TCP :	begin
+						regi[rd] = (~regi[rs] + 1);
+					end	//3'b101
+					`FUNC_SHL : begin
+						regi[rd] = (regi[rs] << 1);
+					end		//3'b110
+					`FUNC_SHR : begin
+						regi[rd] = ARS(regi[rs]);
+					end	//3'b111
+					//`INST_FUNC_ADD : 	//6'd0
+					//`INST_FUNC_SUB :	//6'd1
+					//`INST_FUNC_AND :	//6'd2
+					//`INST_FUNC_ORR :	//6'd3
+					//`INST_FUNC_NOT :	//6'd4
+					//`INST_FUNC_TCP :	//6'd5
+					//`INST_FUNC_SHL :	//6'd6
+					//`INST_FUNC_SHR :	//6'd7
+					`INST_FUNC_JPR : begin
+						pc = regi[rs];
+					end	//6'd25
+					`INST_FUNC_JRL : begin
+						regi[2] = pc;
+						pc = regi[rs];
+					end	//6'd26
+				endcase
+			end	//4'd15 ALU, JPR, JRL
+		endcase
+	end
 endmodule
