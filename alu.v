37d19a3e2b621dd8bd3fb5c8b97a7580c362b1d8
SeungHyun Chae <seunghyunchae@SeungHyuns-MacBook-Air.local>
2020-04-19 00:48:25 +0900

    imm part update

---
+++ alu.v
@@ -0,0 +1,61 @@
+`include "opcodes.v"
+
+module alu(
+	func,
+	data_1,
+	data_2,
+
+	zero,
+	ALU_result
+);
+
+	input [2:0] func;
+	input [`WORD_SIZE - 1:0] data_1;
+	input [`WORD_SIZE - 1:0] data_2;
+
+	output [`WORD-SIZE - 1:0] ALU_result;
+
+	reg [`WORD_SIZE - 1:0] ALU_result;
+
+	initial begin
+		ALU_result = 0;
+	end
+
+	always @(*) begin
+		case(func)
+			`FUNC_ADD: begin
+				ALU_result = data_1 + data_2;
+			end
+
+			`FUNC_SUB: begin 
+				ALU_result = data_1 - data_2; 
+			end
+			
+			`FUNC_NOT: begin 
+				ALU_result = ~data_1; 
+			end
+			
+			`FUNC_AND: begin 
+				ALU_result = data_1 & data_2; 
+			end
+
+			`FUNC_ORR: begin 
+				ALU_result = data_1 | data_2; 
+			end
+			
+			`FUNC_TCP: begin 
+				ALU_result = ~(data_1) + 1; 
+			end
+			
+			`FUNC_SHL: begin 
+				ALU_result = data_1 << 1; 
+			end
+			
+			`FUNC_SHR: begin 
+				ALU_result = data_1 >> 1; 
+			end
+		endcase
+
+		zero = (ALU_result == 0) ? 1 : 0;
+	end
+endmodule
--- cpu.v
+++ cpu.v
@@ -11,42 +11,49 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	input reset_n;
 	input clk;
 
+	// CPU output
 	reg readM;
 	reg writeM;
 	reg [`WORD_SIZE -1:0] address;
+
+	// instruction sub parts
 	reg [3:0]opcode;
 	reg [1:0]rs;
 	reg [1:0]rt;
 	reg [1:0]rd;
 	reg [5:0]func;
 	reg [7:0]imm;
-	reg [11:0]jumpimm;
-	reg [`WORD_SIZE -1 :0]se;
-	reg [`WORD_SIZE - 1:0]regi[3:0];
-	reg [`WORD_SIZE - 1:0]pc;
+	reg [11:0]target_address;
+
+	wire [`WORD_SIZE - 1:0] extended_imm;
+	imm_generator immGen1(imm, extended_imm)
+
+	reg [`WORD_SIZE - 1:0] registers[3:0];
+	reg [`WORD_SIZE - 1:0] pc;
 	reg write_data;
+
 	integer i;
-
-	function [`WORD_SIZE-1:0]ARS;
-		input [`WORD_SIZE-1:0]A;
-		begin
-			if(A[`WORD_SIZE-1]==1)
-				ARS=((A>>1)+16'h8000);
-			else
-				ARS=(A>>1);
-		end
-	endfunction
-
 	initial begin
+		address = 16'h0000;
 		pc = 16'h000;
 		for (i = 0; i< 4; i = i + 1)begin
-			regi[i] = 16'h0000;
+			registers[i] = 16'h0000;
 		end
-		address = 16'h0000;
 		write_data = 0;
 	end
 
-	assign data = write_data ? regi[rt] : `WORD_SIZE'bz;
+	assign data = write_data ? registers[rt] : `WORD_SIZE'bz;
+
+	/*
+	wire [2:0] ALUOp;
+	wire Regst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
+	control_unit control_unit1(opcode, func, RegDst, Jump, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
+
+
+	wire zero;
+	wire[`WORD_SIZE] ALU_result;
+	alu ALU(ALUOp, data_1, data_2, zero, ALU_reult);
+	*/
 
 	integer state; //state Identifier
 
@@ -62,149 +69,113 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	end
 
 	always @(ackOutput) begin
-	if(ackOutput == 1)
-		state = 5; //write memory
-	if(ackOutput == 0)
-		state = 3; //empty state(final state)
+		if(ackOutput == 1)
+			state = 5; //write memory
+		if(ackOutput == 0)
+			state = 3; //empty state(final state)
 	end
 
 	always @(state) begin
-	if(state == 1)
-		begin
-		readM = 0;
-		opcode = data[`WORD_SIZE - 1: 12];
-		rs = data[11:10];
-		rt = data[9:8];
-		rd = data[7:6];
-		func = data[5:0];
-		imm = data[7:0];
-		jumpimm = data[11:0];
-		se = 16'h0000;
-		pc = pc + 1;
+		if(state == 1) begin // I stage
+			readM = 0;
+			pc = pc + 1;
+
+			opcode = data[15:12];
+			rs = data[11:10];
+			rt = data[9:8];
+			rd = data[7:6];
+			func = data[5:0];
+			imm = data[7:0];
+			target_address = data[11:0];
+
+			se = 16'h0000;
 		end
-	if(state == 2)
-		begin
-		case(opcode)
+
+		if(state == 2) begin
+			case(opcode)
 			`ADI_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				regi[rt] = (regi[rs] + se);
+				registers[rt] = (registers[rs] + extended_imm);
 				end//4'd4
+
 			`ORI_OP	:begin
-				se = (se | imm);
-				regi[rt] = (regi[rs] | se);
-				end	//4'd5
+				registers[rt] = (registers[rs] | $signed(imm));
+			end	//4'd5
+
 			`LHI_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				regi[rt] = se;
-				end	//4'd6
+				registers[rt] = ($signed(imm) << 8);
+			end	//4'd6
+
 			`LWD_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				address = (regi[rs] + se);
+				address = (registers[rs] + extended_imm);
 				readM = 1;
 			end	//4'd7
+
 			`SWD_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				address = (regi[rs] + se);
+				address = (registers[rs] + extended_imm);
 				write_data = 1;
 				writeM = 1;
-				end	//4'd8
+			end	//4'd8
+
 			`BNE_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				if(regi[rs] != regi[rt])begin
-					pc = (pc + se);
+				if(registers[rs] != registers[rt]) begin
+					pc = (pc + extended_imm);
 				end
 			end	//4'd0
+
 			`BEQ_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				if(regi[rs] == regi[rt])begin
-					pc = (pc + se);
+				if(registers[rs] == registers[rt]) begin
+					pc = (pc + extended_imm);
 				end
 			end	//4'd1
+
 			`BGZ_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				if( (regi[rs][`WORD_SIZE - 1] == 0) && (regi[rs] != 0) )begin
-					pc = (pc + se);
+				if((registers[rs][`WORD_SIZE - 1] == 0) && (registers[rs] != 0)) begin
+					pc = (pc + extended_imm);
 				end
 			end	//4'd2
+
 			`BLZ_OP	:begin
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
-				if( (regi[rs][`WORD_SIZE - 1] == 1) && (regi[rs] != 0) )begin
-					pc = (pc + se);
+				if((registers[rs][`WORD_SIZE - 1] == 1) && (registers[rs] != 0)) begin
+					pc = (pc + extended_imm);
 				end
 			end	//4'd3
+
 			`JMP_OP	:begin
-				se = (se | jumpimm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
 				pc = (pc & 16'hf000);
-				pc = (pc | se);
+				pc = (pc | $signed(target_address));
 			end	//4'd9
+
 			`JAL_OP :begin
-				regi[2] = pc;
-				se = (se | imm);
-				se = (se << 8);
-				for(i = 0; i< 8; i = i + 1) begin
-					se = ARS(se);
-				end
+				registers[2] = pc;
 				pc = (pc & 16'hf000);
-				pc = (pc | se);
+				pc = (pc | extended_imm);
 			end	//4'd10
+
 			default :begin
 				case(func)
 					`FUNC_ADD : begin
-						regi[rd] = (regi[rs] + regi[rt]);
+						registers[rd] = (registers[rs] + registers[rt]);
 					end	//3'b000
 					`FUNC_SUB : begin
-						regi[rd] = (regi[rs] - regi[rt]);
+						registers[rd] = (registers[rs] - registers[rt]);
 					end //3'b001
 					`FUNC_AND : begin
-						regi[rd] = (regi[rs] & regi[rt]);
+						registers[rd] = (registers[rs] & registers[rt]);
 					end	//3'b010
 					`FUNC_ORR :	begin
-						regi[rd] = (regi[rs] | regi[rt]);
+						registers[rd] = (registers[rs] | registers[rt]);
 					end//3'b011
 					`FUNC_NOT :	begin
-						regi[rd] = ~regi[rs];
+						registers[rd] = ~registers[rs];
 					end	//3'b100
 					`FUNC_TCP :	begin
-						regi[rd] = (~regi[rs] + 1);
+						registers[rd] = (~registers[rs] + 1);
 					end	//3'b101
 					`FUNC_SHL : begin
-						regi[rd] = (regi[rs] << 1);
+						registers[rd] = (registers[rs] << 1);
 					end		//3'b110
 					`FUNC_SHR : begin
-						regi[rd] = ARS(regi[rs]);
+						registers[rd] = ARS(registers[rs]);
 					end	//3'b111
 					//`INST_FUNC_ADD : 	//6'd0
 					//`INST_FUNC_SUB :	//6'd1
@@ -215,24 +186,24 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 					//`INST_FUNC_SHL :	//6'd6
 					//`INST_FUNC_SHR :	//6'd7
 					`INST_FUNC_JPR : begin
-						pc = regi[rs];
+						pc = registers[rs];
 					end	//6'd25
 					`INST_FUNC_JRL : begin
-						regi[2] = pc;
-						pc = regi[rs];
+						registers[2] = pc;
+						pc = registers[rs];
 					end	//6'd26
 				endcase
 			end	//4'd15 ALU, JPR, JRL
 		endcase
 		state = 3;
 		end
-	if(state == 4)
-		begin
+
+	if(state == 4) begin
 		readM = 0;
-		regi[rt] = data;
+		registers[rt] = data;
 		end
-	if(state == 5)
-		begin
+
+	if(state == 5) begin
 		writeM = 0;
 		write_data = 0;
 		end
