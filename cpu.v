475fc426a1330ace6d431e4c8ac5d9ae3e776e79
shchae7 <seunghyunchae@chaeseunghyeons-MacBook-Pro.local>
2020-04-19 15:54:48 +0900

    should work

--- cpu.v
+++ cpu.v
@@ -16,6 +16,8 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	reg writeM;
 	reg [`WORD_SIZE -1:0] address;
 
+    reg [`WORD_SIZE - 1:0] pc;
+
 	// instruction sub parts
 	reg [3:0] opcode;
 	reg [1:0] rs;
@@ -29,13 +31,12 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	reg [`WORD_SIZE - 1:0] extended_target;
 
 	reg [`WORD_SIZE - 1:0] registers[3:0];
-	reg [`WORD_SIZE - 1:0] pc;
 	reg write_data;
 
 	integer i;
 	initial begin
 		address = 16'h0000;
-		pc = 16'h000;
+		pc = 16'h0000;
 		for (i = 0; i< 4; i = i + 1) begin
 			registers[i] = 16'h0000;
 		end
@@ -78,166 +79,167 @@ module cpu (readM, writeM, address, data, ackOutput, inputReady, reset_n, clk);
 	reg [`WORD_SIZE - 1:0] extended_temp;
 
 	always @(state) begin
-		if(state == 1) begin // I stage
-			readM = 0;
-			pc = pc + 1;
+	if(state == 1) begin // I stage
+		readM = 0;
+		pc = pc + 1;
 
-			opcode = data[15:12];
-			rs = data[11:10];
-			rt = data[9:8];
-			rd = data[7:6];
-			func = data[5:0];
-			imm = data[7:0];
-			target_address = data[11:0];
+		opcode = data[15:12];
+		rs = data[11:10];
+		rt = data[9:8];
+		rd = data[7:6];
+		func = data[5:0];
+		imm = data[7:0];
+		target_address = data[11:0];
+        extended_imm = 16'h0000;
+        extended_target = 16'h0000;
 
-			extended_temp = 16'h0000;
-		end
+		extended_temp = 16'h0000;
+	    end
 
-		if(state == 2) begin
-			extended_imm = 16'h0000;
-			extended_imm = (extended_imm | imm) << 8;
-			for(i = 0; i< 8; i = i + 1) begin
-				extended_imm = extended_imm >> 1;
-				if(extended_imm[15] == 1)
-					extended_imm = extended_imm + 16'h8000;
+	if(state == 2) begin
+		extended_imm = (extended_imm | imm) << 8;
+		for(i = 0; i< 8; i = i + 1) begin
+			if(extended_imm[15] == 1)
+				extended_imm = (extended_imm >> 1) + 16'h8000;
+            else
+                extended_imm = extended_imm >> 1;
 
-			extended_target = 16'h0000;
-			extended_target = (extended_target | target_address) << 8;
-			for(i = 0; i< 8; i = i + 1) begin
-				extended_target = extended_target >> 1;
-				if(extended_target[15] == 1)
-				extended_target = extended_target + 16'h8000;
+		extended_target = (extended_target | target_address) << 8;
+		for(i = 0; i< 8; i = i + 1) begin
+			if(extended_target[15] == 1)
+			    extended_target = (extended_target >> 1) + 16'h8000;
+            else
+                extended_target = extended_target >> 1;
 
-			if(opcode == `ALU_OP) // R-type
-				data_1 = registers[rs];
-				data_2 = registers[rt];
+		if(opcode == `ALU_OP) // R-type
+			data_1 = registers[rs];
+			data_2 = registers[rt];
 
-				case(func)
-				`INST_FUNC_ADD : begin
-					ALUOp = `FUNC_ADD;
-					registers[rd] = ALU_result;
+			case(func)
+			`INST_FUNC_ADD : begin
+				ALUOp = `FUNC_ADD;
+				registers[rd] = ALU_result;
+			    end
+
+			`INST_FUNC_SUB : begin
+				ALUOp = `FUNC_SUB;
+				registers[rd] = ALU_result;
 				end
 
-				`INST_FUNC_SUB : begin
-					ALUOp = `FUNC_SUB;
-					registers[rd] = ALU_result;
+			`INST_FUNC_AND : begin
+				ALUOp = `FUNC_AND;
+				registers[rd] = ALU_result;
 				end
 
-				`INST_FUNC_AND : begin
-					ALUOp = `FUNC_AND;
-					registers[rd] = ALU_result;
+			`INST_FUNC_ORR : begin
+				ALUOp = `FUNC_ORR;
+				registers[rd] = ALU_result;
 				end
 
-				`INST_FUNC_ORR : begin
-					ALUOp = `FUNC_ORR;
-					registers[rd] = ALU_result;
-				end
-
-				`INST_FUNC_NOT : begin
-					ALUOp = `FUNC_NOT;
-					registers[rd] = ALU_result;
+			`INST_FUNC_NOT : begin
+				ALUOp = `FUNC_NOT;
+				registers[rd] = ALU_result;
 				end
 				
-				`INST_FUNC_TCP : begin
-					ALUOp = `FUNC_TCP;
-					registers[rd] = ALU_result;
+			`INST_FUNC_TCP : begin
+				ALUOp = `FUNC_TCP;
+				registers[rd] = ALU_result;
 				end
 
-				`INST_FUNC_SHL : begin
-					ALUOp = `FUNC_SHL;
-					registers[rd] = ALU_result;
-                    		end
+			`INST_FUNC_SHL : begin
+				ALUOp = `FUNC_SHL;
+				registers[rd] = ALU_result;
+                end
 
-				`INST_FUNC_SHR : begin
-					ALUOp = `FUNC_SHR;
-					registers[rd] = ALU_result;
-					if(data_1[15] == 1)
-						registers[rd] = registers[rd] + 16'h8000;
+			`INST_FUNC_SHR : begin
+				ALUOp = `FUNC_SHR;
+				registers[rd] = ALU_result;
+				if(data_1[15] == 1)
+					registers[rd] = registers[rd] + 16'h8000;
 				end
 
-				`INST_FUNC_JPR : begin
-					pc = data_1;
+			`INST_FUNC_JPR : begin
+				pc = data_1;
 				end
 
-				`INST_FUNC_JRL : begin
-					registers[2] = pc;
-					pc = data_1;
+			`INST_FUNC_JRL : begin
+				registers[2] = pc;
+				pc = data_1;
 				end
-				endcase
+			endcase
 
-			else
-				case(opcode) // I, J-type
-				`ADI_OP	: begin
-					$display("ADI operation");
-					registers[rt] = (registers[rs] + extended_imm);
-				end
-
-				`ORI_OP	: begin
-					$display("ORI operation");
-					extended_temp = (extended_temp | imm);
-					registers[rt] = registers[rs] | extended_temp;
+        else begin
+			case(opcode) // I, J-type
+			`ADI_OP	: begin
+				$display("ADI operation");
+				registers[rt] = (registers[rs] + extended_imm);
 				end
 
-				`LHI_OP	: begin
-					$display("LHI operation");
-					extended_temp = (extended_temp | imm);
-					registers[rt] = (extended_temp << 8);
+			`ORI_OP	: begin
+				$display("ORI operation");
+				extended_temp = (extended_temp | imm);
+				registers[rt] = registers[rs] | extended_temp;
+				end
+
+			`LHI_OP	: begin
+				$display("LHI operation");
+				extended_temp = (extended_temp | imm);
+				registers[rt] = (extended_temp << 8);
 				end
 	
-				`LWD_OP	:begin
-					$display("LWD operation");
-					address = (registers[rs] + extended_imm);
-					readM = 1;
+			`LWD_OP	:begin
+				$display("LWD operation");
+				address = (registers[rs] + extended_imm);
+				readM = 1;
 				end
 	
-				`SWD_OP	:begin
-					$display("SWD operation");
-					address = (registers[rs] + extended_imm);
-					write_data = 1;
-					writeM = 1;
+			`SWD_OP	:begin
+				$display("SWD operation");
+				address = (registers[rs] + extended_imm);
+				write_data = 1;
+				writeM = 1;
 				end
 	
-				`BNE_OP	:begin
-					$display("BNE operation");
-					if(registers[rs] != registers[rt]) begin
-						pc = (pc + extended_imm);
+			`BNE_OP	:begin
+				$display("BNE operation");
+				if(registers[rs] != registers[rt]) begin
+					pc = (pc + extended_imm);
+				    end
+		    	end
+	
+			`BEQ_OP	:begin
+				$display("BEQ operation");
+				if(registers[rs] == registers[rt]) begin
+					pc = (pc + extended_imm);
 					end
+            	end
+	
+			`BGZ_OP	:begin
+				$display("BGZ operation");
+				if((registers[rs][`WORD_SIZE - 1] == 0) && (registers[rs] != 0)) begin
+					pc = (pc + extended_imm);
+				    end
+			    end
+	
+			`BLZ_OP	:begin
+				$display("BLZ operation");
+				if((registers[rs][`WORD_SIZE - 1] == 1) && (registers[rs] != 0)) begin
+					pc = (pc + extended_imm);
+			    	end
 				end
 	
-				`BEQ_OP	:begin
-					$display("BEQ operation");
-					if(registers[rs] == registers[rt]) begin
-						pc = (pc + extended_imm);
-					end
-            			end
-	
-				`BGZ_OP	:begin
-					$display("BGZ operation");
-					if((registers[rs][`WORD_SIZE - 1] == 0) && (registers[rs] != 0)) begin
-						pc = (pc + extended_imm);
-					end
+			`JMP_OP	:begin
+				pc = (pc & 16'hf000);
+				pc = (pc | extended_target);
 				end
 	
-				`BLZ_OP	:begin
-					$display("BLZ operation");
-					if((registers[rs][`WORD_SIZE - 1] == 1) && (registers[rs] != 0)) begin
-						pc = (pc + extended_imm);
-					end
+			`JAL_OP :begin
+				registers[2] = pc;
+				pc = (pc & 16'hf000);
+				pc = (pc | extended_imm);
 				end
-	
-				`JMP_OP	:begin
-					pc = (pc & 16'hf000);
-					pc = (pc | extended_target);
-				end
-	
-				`JAL_OP :begin
-					registers[2] = pc;
-					pc = (pc & 16'hf000);
-					pc = (pc | extended_imm);
-				end
-	
-				endcase	
-
+			endcase	
+            end
 		state = 3;
 		end
 
